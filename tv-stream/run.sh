d0790c7d61460b88d7049462309c895c38f7c598
Pascal Wächter <pascalwaechter@wae-mail.de>
2022-08-10 18:20:30 +0200

    v0.1.18

--- tv-stream/run.sh
+++ tv-stream/run.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-ls -la /darkice.cfg
-
 echo "Starting icecast service"
 icecast -b -c /etc/icecast.xml 
 
 echo "Starting darkice service"
 darkice -c /darkice.cfg
+
