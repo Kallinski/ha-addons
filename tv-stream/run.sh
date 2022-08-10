a7a7e2380f50c164303d86d1bbff6acc06e5966a
Pascal Wächter <pascalwaechter@wae-mail.de>
2022-08-10 17:50:13 +0200

    v0.1.15

--- tv-stream/run.sh
+++ tv-stream/run.sh
@@ -5,7 +5,7 @@ which icecast
 cat /etc/icecast.xml
 
 echo "Starting icecast service"
-icecast -c /etc/icecast.xml 
+icecast -b -c /etc/icecast.xml 
 
 echo "Starting darkice service"
 darkice -c /root/darkice.cfg
